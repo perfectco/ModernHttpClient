2a338a58ae2a3df641cfe2f03fedf3db43105538
Mark Harris <mark@markharris.net.au>
2018-02-19 19:10:33 +1100

    fixed nuget package building

--- buildNuget.ps1
+++ buildNuget.ps1
@@ -1,9 +1,10 @@
 msbuild ./ /t:Build /p:Configuration=Release
-mkdir -p ./build/MonoAndroid
+Get-ChildItem -Path ./build -Include *.* -File -Recurse | foreach { $_.Delete()}
+mkdir -p ./build/MonoAndroid  -Force
 move-item ./src/ModernHttpClient/bin/Release/MonoAndroid/Modern* ./build/MonoAndroid -force
-mkdir -p ./build/Xamarin.iOS10
+mkdir -p ./build/Xamarin.iOS10  -Force
 move-item ./src/ModernHttpClient/bin/Release/Xamarin.iOS10/Modern* ./build/Xamarin.iOS10 -force
-mkdir -p ./build/Portable-Net45+WinRT45+WP8+WPA81
+mkdir -p ./build/Portable-Net45+WinRT45+WP8+WPA81  -Force
 move-item ./src/ModernHttpClient/bin/Release/Portable-Net45+WinRT45+WP8+WPA81/Modern* ./build/Portable-Net45+WinRT45+WP8+WPA81 -force
 
 rm *.nupkg
